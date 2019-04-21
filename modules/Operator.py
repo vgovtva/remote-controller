abab47ed58f0061c8365ca4d8f841d80a3b7a72c
Vladislav Govtva <vlad.govtva@gmail.com>
2019-04-21 03:25:41 +0300

    actionset.py: add skeliton for layout file
    Signed-off-by: Vladislav Govtva <vlad.govtva@gmail.com>

--- modules/Operator.py
+++ modules/Operator.py
@@ -3,8 +3,26 @@
 # vim: ts=4 sw=4 tw=100 et ai si
 
 import yaml
+from modules import actionset
+
+
+def _is_action(func_name):
+    """Helper function to check action is out of 'actionset'."""
+
+    func = getattr(actionset, func_name, None)
+    if not func:
+        return False
+    return callable(func) and func.__module__ == actionset.__name__
 
 class Operator:
+
+    def _get_action(self, code, mode):
+        """
+        Get the name of function from layout based on button 'code' and current 'mode' the layout is in.
+        """
+
+        binds = self._layout.get(code, None)
+        return binds.get(mode, None) if binds else None
 
     def _load_layout(self, path):
         """load the layout configuration for the controller."""
@@ -21,11 +39,17 @@ class Operator:
     def run(self):
         """Read device input and send it."""
 #        self._connection.send("hello there")
+        mode = 1
         for code, value in self._device.read():
-            print(code, value)
+            func_name = self._get_action(code, mode)
+            func = self.actions.get(func_name, actionset.nothing) if func_name else actionset.nothing
+            ret = func(value)
 
     def __init__(self, connection, device, layout):
 
         self._connection = connection
         self._device = device
         self._layout = self._load_layout(layout)
+        self.actions = {}
+
+        self.actions = {func:getattr(actionset, func) for func in dir(actionset) if _is_action(func)}
---
+++ modules/actionset.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+
+def nothing(val):
+    """Default function. Does nothing."""
+
+    return None
+
+def hello_world(val):
+    """Hellow world function."""
+
+    if val:
+        print("hello world!")
+    return 0
