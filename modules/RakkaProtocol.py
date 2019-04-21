8b59c24cb425aef84291fbe8a27a079a5512bed8
Vladislav Govtva <vlad.govtva@gmail.com>
2019-04-21 03:57:38 +0300

    RakkaProtocol.py: create skeleton protocol
    Signed-off-by: Vladislav Govtva <vlad.govtva@gmail.com>

---
+++ modules/RakkaProtocol.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+
+"""A module that contains a class for converting a dict to Rakkatec protocol."""
+
+class RakkaProtocol:
+
+    @staticmethod
+    def to_bytes(control_dict):
+        """
+        A method that converts a dictionary with control intructions/values to a bytes object to
+        control Rakka 3000 vehicle.
+        """
+        return bytes(0)
+
